ef6f7c2fc563ee60e25f94677837d18743be1bc9
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-05-18 20:04:56 +0600

    added single id get request route

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ async function run() {
       const result = await toyCollection.deleteOne(query);
       res.send(result);
     });
+    app.get("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      //   console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.findOne(query);
+      res.send(result);
+    });
     app.put("/toys/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
